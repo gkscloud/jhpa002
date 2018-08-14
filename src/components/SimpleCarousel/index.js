451159c0fd2e451883f21add710cc24ea2f01d11
G.K <gibran.kichlu@pnglabs.com>
2018-08-14 15:45:58 -0400

    build failed because LightBox import needs to be done inside componentDidMount only:

--- src/components/SimpleCarousel/index.js
+++ src/components/SimpleCarousel/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import LightBox from 'react-image-lightbox'
-
 
 class SimpleCarouselTemplate extends React.Component {
     constructor(props){
