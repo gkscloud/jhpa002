560aad10b48d96fffedbfc6c6f83c0f8647de5b5
G.K <gibran.kichlu@pnglabs.com>
2018-08-16 00:18:54 -0400

    Change 404 default message

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,17 +5,17 @@ import React from 'react'
 
 const NotFoundPage = () => (
   <div>
-    <section className='hero is-primary is-bold is-large'>
+    <section className='hero is-light is-bold is-large'>
       <div className='hero-body'>
         <div className='container'>
           <div className='columns'>
             <div className='column is-10 is-offset-1'>
               <div className='section is-centered'>
                 <h1 className='title'>
-                                    404: NOT FOUND
+                                  404: NOT FOUND
                 </h1>
-                <h2 className='subtitle'>
-                                    You just hit a route that doesn&#39;t exist... the
+                <h2 className='subtitle is-dark'>
+                                    Oops! You just hit a route that doesn&#39;t exist... the
                                     sadness.
                 </h2>
               </div>
